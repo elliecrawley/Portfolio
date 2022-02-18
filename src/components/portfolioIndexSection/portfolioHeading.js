0f67eb960071fba818fbf04739cbe186092e0071
Ellie Crawley <elliecrawley@hotmail.co.uk>
2022-02-18 12:09:07 +0000

    change projects

--- src/components/portfolioIndexSection/portfolioHeading.js
+++ src/components/portfolioIndexSection/portfolioHeading.js
@@ -11,15 +11,9 @@ const PortfolioHeading = () => {
                 <Link to="/Emily-Carter" className="portfolio-item" aria-label="Link to 'Emily Carter' project page">
                     <PortfolioItems heading='Emily Carter' text="Shopify store created for Emily Carter, an award-winning British designer specialising in hand illustrated silk accessories, stationery and furnishings." classHidden="hidden" languages="Liquid, JavaScript, HTML5, CSS3" src='images/ECthumbnail.png' alt="Emily Carter Homepage"/>
                 </Link>
-                <Link to="/WineSpark" className="portfolio-item" aria-label="Link to 'WineSpark' project page">
-                    <PortfolioItems heading='WineSpark' text="Shopify store created for WineSpark, a UK based wine subscription service." languages="Liquid, JavaScript, HTML5, CSS3" classHidden="hidden" src='images/WineSparkthumbnail.jpg' alt="WineSpark Homepage"/>
-                </Link>
-                <Link to="/Cultured-Biomecare" className="portfolio-item" aria-label="Link to 'Cultured Biomecare' project page">
-                    <PortfolioItems heading='Cultured Biomecare' text="Shopify store created for Cultured Biomecare, an award-winning UK based skin care brand." languages="Liquid, JavaScript, HTML5, CSS3" classHidden="hidden" src='images/Culturedthumbnail.png' alt="Cultured Biomecare Homepage"/>
-                </Link>
-                <Link to="/Avery-Row" className="portfolio-item" aria-label="Link to 'Avery Row' project page">
+                {/* <Link to="/Avery-Row" className="portfolio-item" aria-label="Link to 'Avery Row' project page">
                     <PortfolioItems heading='Avery Row' text="Shopify store created for Avery Row, an independent British brand specialising in organic, natural textiles and accessories for children." languages="JavaScript, HTML5, CSS3, Liquid" programmes="Klaviyo" src='images/AveryRowThumbnail.png' alt="Avery Row Homepage"/>
-                </Link>
+                </Link> */}
                 {/* <Link to="/ForestWise" className="portfolio-item">
                     <PortfolioItems heading='ForestWise' text="A concept piece designing the homepage for a fictional 'forest school' business." languages="JavaScript, HTML5, CSS3, PHP" programmes="InkScape, AdobeXD, ScrollMagic, GSAP" src='images/ForestWisethumbnail.png'/>
                 </Link> */}
