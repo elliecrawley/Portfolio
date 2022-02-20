629e21861b11168b085bb5d4c8be8a2d37eb50c4
Ellie Crawley <elliecrawley@hotmail.co.uk>
2022-02-20 12:51:34 +0000

    styling

--- src/components/heroSection/heroImage.js
+++ src/components/heroSection/heroImage.js
@@ -4,8 +4,8 @@ import PortPic from "../../assets/images/PortPic.png"
 const HeroImage = () => {
     return (
         <> 
-            <div className="hero-img hero-img-mobile hidden-desktop" alt="Ellie Crawley"/>
-            <img className="hero-img hero-img-desktop hidden-mobile" src={PortPic}/>
+            <div className="hero-img hero-img-mobile" alt="Ellie Crawley"/>
+            <img className="hero-img hero-img-desktop" src={PortPic}/>
         </>
     )
 }
